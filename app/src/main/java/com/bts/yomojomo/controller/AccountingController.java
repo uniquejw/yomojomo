50ce1669cac7153d4c13ef0ceb9e3395f0b49ed9
KyoungHyunPark <findkh@nate.com>
2022-03-16 18:27:24 +0900

    .

---
+++ app/src/main/java/com/bts/yomojomo/controller/AccountingController.java
@@ -0,0 +1,38 @@
+package com.bts.yomojomo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.bts.yomojomo.dao.AccountingDao;
+import com.bts.yomojomo.domain.Accounting;
+
+@RestController
+public class AccountingController {
+  @Autowired 
+  AccountingDao accountingDao;
+
+  @RequestMapping("/accounting/add")
+  public int add(Accounting accounting) {
+    return accountingDao.insert(accounting);
+  }
+
+  @RequestMapping("/accounting/get")
+  public Object get(int no) {
+    return accountingDao.findByNo(no);
+  }
+
+  @RequestMapping("/accounting/list")
+  public Object list() {
+    return accountingDao.findAll(); 
+  }
+
+  @RequestMapping("/accounting/update")
+  public int update(Accounting accounting) {
+    return accountingDao.update(accounting);
+  }
+
+  @RequestMapping("/accounting/delete")
+  public Object delete(int no) {
+    return accountingDao.delete(no);
+  }
+}
---
+++ app/src/main/java/com/bts/yomojomo/domain/Accounting.java
@@ -0,0 +1,62 @@
+package com.bts.yomojomo.domain;
+
+import java.sql.Date;
+
+public class Accounting {
+  int no;
+  int gno;
+  int actcateno;
+  String title;
+  int amount;
+  Date date;
+  int status;
+
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public int getGno() {
+    return gno;
+  }
+  public void setGno(int gno) {
+    this.gno = gno;
+  }
+  public int getActcateno() {
+    return actcateno;
+  }
+  public void setActcateno(int actcateno) {
+    this.actcateno = actcateno;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public int getAmount() {
+    return amount;
+  }
+  public void setAmount(int amount) {
+    this.amount = amount;
+  }
+  public Date getDate() {
+    return date;
+  }
+  public void setDate(Date date) {
+    this.date = date;
+  }
+  public int getStatus() {
+    return status;
+  }
+  public void setStatus(int status) {
+    this.status = status;
+  }
+
+  @Override
+  public String toString() {
+    return "Accounting [no=" + no + ", gno=" + gno + ", actcateno=" + actcateno + ", title=" + title
+        + ", amount=" + amount + ", date=" + date + ", status=" + status + "]";
+  }
+}
