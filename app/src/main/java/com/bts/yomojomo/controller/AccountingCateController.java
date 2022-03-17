d03ca75b379888344e20b09b982b6a44be0ee43f
KyoungHyunPark <findkh@nate.com>
2022-03-17 15:46:24 +0900

    .

--- app/src/main/java/com/bts/yomojomo/controller/AccountingCateController.java
+++ app/src/main/java/com/bts/yomojomo/controller/AccountingCateController.java
@@ -1,38 +1,38 @@
-//package com.bts.yomojomo.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import com.bts.yomojomo.dao.AccountingCateDao;
-//import com.bts.yomojomo.domain.AccountingCate;
-//
-//@RestController
-//public class AccountingCateController {
-//  @Autowired
-//  AccountingCateDao accountingcateDao;
-//
-//  @RequestMapping("/accountingcate/add")
-//  public Object add(AccountingCate accountingCate) {
-//    return accountingcateDao.insert(accountingCate);
-//  }
-//
-//  @RequestMapping("/accountingcate/get")
-//  public Object get(int no) {
-//    return accountingcateDao.findByNo(no);
-//  }
-//
-//  @RequestMapping("/accountingcate/list")
-//  public Object list() {
-//    return accountingcateDao.findAll(); 
-//  }
-//
-//  @RequestMapping("/accountingcate/update")
-//  public Object update(AccountingCate accountingCate) {
-//    return accountingcateDao.update(accountingCate);
-//  }
-//
-//  @RequestMapping("/accountingcate/delete")
-//  public Object delete(int no) {
-//    return accountingcateDao.delete(no);
-//  }
-//}
+package com.bts.yomojomo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.bts.yomojomo.dao.AccountingCateDao;
+import com.bts.yomojomo.domain.AccountingCate;
+
+@RestController
+public class AccountingCateController {
+  @Autowired
+  AccountingCateDao accountingcateDao;
+
+  @RequestMapping("/accountingcate/add")
+  public Object add(AccountingCate accountingCate) {
+    return accountingcateDao.insert(accountingCate);
+  }
+
+  @RequestMapping("/accountingcate/get")
+  public Object get(int no) {
+    return accountingcateDao.findByNo(no);
+  }
+
+  @RequestMapping("/accountingcate/list")
+  public Object list() {
+    return accountingcateDao.findAll(); 
+  }
+
+  @RequestMapping("/accountingcate/update")
+  public Object update(AccountingCate accountingCate) {
+    return accountingcateDao.update(accountingCate);
+  }
+
+  @RequestMapping("/accountingcate/delete")
+  public Object delete(int no) {
+    return accountingcateDao.delete(no);
+  }
+}
--- app/src/main/java/com/bts/yomojomo/dao/AccountingDao.java
+++ app/src/main/java/com/bts/yomojomo/dao/AccountingDao.java
@@ -1,20 +1,20 @@
-//package com.bts.yomojomo.dao;
-//
-//import java.util.List;
-//import org.apache.ibatis.annotations.Mapper;
-//import com.bts.yomojomo.domain.Accounting;
-//
-//@Mapper 
-//public interface AccountingDao {
-//  int countAll();
-//
-//  List<Accounting> findAll();
-//
-//  int insert(Accounting accounting);
-//
-//  Accounting findByNo(int no);
-//
-//  int update(Accounting accounting);
-//
-//  int delete(int no);
-//}
+package com.bts.yomojomo.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import com.bts.yomojomo.domain.Accounting;
+
+@Mapper 
+public interface AccountingDao {
+  int countAll();
+
+  List<Accounting> findAll();
+
+  int insert(Accounting accounting);
+
+  Accounting findByNo(int no);
+
+  int update(Accounting accounting);
+
+  int delete(int no);
+}
