109a6690761a16a8832c77d71c3b9720c73af707
KyoungHyunPark <findkh@nate.com>
2022-03-19 14:40:12 +0900

    .

--- app/src/main/java/com/bts/yomojomo/domain/Accounting.java
+++ app/src/main/java/com/bts/yomojomo/domain/Accounting.java
@@ -1,5 +1,7 @@
 package com.bts.yomojomo.domain;
 
+import java.sql.Date;
+
 public class Accounting {
   int no;
   int gno;
@@ -7,61 +9,52 @@ public class Accounting {
   String title;
   int amount;
   int status;
-
+  Date date;
   @Override
   public String toString() {
     return "Accounting [no=" + no + ", gno=" + gno + ", actcateno=" + actcateno + ", title=" + title
-        + ", amount=" + amount + ", status=" + status + "]";
+        + ", amount=" + amount + ", status=" + status + ", date=" + date + "]";
   }
-
   public int getNo() {
     return no;
   }
-
   public void setNo(int no) {
     this.no = no;
   }
-
   public int getGno() {
     return gno;
   }
-
   public void setGno(int gno) {
     this.gno = gno;
   }
-
   public int getActcateno() {
     return actcateno;
   }
-
   public void setActcateno(int actcateno) {
     this.actcateno = actcateno;
   }
-
   public String getTitle() {
     return title;
   }
-
   public void setTitle(String title) {
     this.title = title;
   }
-
   public int getAmount() {
     return amount;
   }
-
   public void setAmount(int amount) {
     this.amount = amount;
   }
-
   public int getStatus() {
     return status;
   }
-
   public void setStatus(int status) {
     this.status = status;
   }
-
-
-
+  public Date getDate() {
+    return date;
+  }
+  public void setDate(Date date) {
+    this.date = date;
+  }
 }
