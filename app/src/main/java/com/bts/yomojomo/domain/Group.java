ffdd200ec1adec2c32dae233369f49c199a72a34
uniquejw <jaewongogo@naver.com>
2022-03-21 07:50:37 +0900

    "jw group update"

--- app/src/main/java/com/bts/yomojomo/domain/Group.java
+++ app/src/main/java/com/bts/yomojomo/domain/Group.java
@@ -4,86 +4,86 @@ import java.sql.Date;
 
 public class Group {
   int no;
-  int itrlocalno;
-  int fileno;
+  int actlocalno;
+  int pupsno;
   String name;
-  Date cdate;
+  Date regdt;
   String logo;
   String intro;
-  int mcnt;
+  int maxcnt;
   int fee;
-  int vcnt;
+  int viewcnt;
   
   
-  @Override
-  public String toString() {
-    return "Group [no=" + no + ", itrlocalno=" + itrlocalno + ", fileno=" + fileno + ", name="
-        + name + ", cdate=" + cdate + ", logo=" + logo + ", intro=" + intro + ", mcnt=" + mcnt
-        + ", fee=" + fee + ", vcnt=" + vcnt + "]";
-  }
-  public int getNo() {
-    return no;
-  }
-  public void setNo(int no) {
-    this.no = no;
-  }
-  public int getItrlocalno() {
-    return itrlocalno;
-  }
-  public void setItrlocalno(int itrlocalno) {
-    this.itrlocalno = itrlocalno;
-  }
-  public int getFileno() {
-    return fileno;
-  }
-  public void setFileno(int fileno) {
-    this.fileno = fileno;
-  }
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public Date getCdate() {
-    return cdate;
-  }
-  public void setCdate(Date cdate) {
-    this.cdate = cdate;
-  }
-  public String getLogo() {
-    return logo;
-  }
-  public void setLogo(String logo) {
-    this.logo = logo;
-  }
-  public String getIntro() {
-    return intro;
-  }
-  public void setIntro(String intro) {
-    this.intro = intro;
-  }
-  public int getMcnt() {
-    return mcnt;
-  }
-  public void setMcnt(int mcnt) {
-    this.mcnt = mcnt;
-  }
-  public int getFee() {
-    return fee;
-  }
-  public void setFee(int fee) {
-    this.fee = fee;
-  }
-  public int getVcnt() {
-    return vcnt;
-  }
-  public void setVcnt(int vcnt) {
-    this.vcnt = vcnt;
-  }
+  
+@Override
+public String toString() {
+	return "Group [no=" + no + ", actlocalno=" + actlocalno + ", pupsno=" + pupsno + ", name=" + name + ", regdt="
+			+ regdt + ", logo=" + logo + ", intro=" + intro + ", maxcnt=" + maxcnt + ", fee=" + fee + ", viewcnt="
+			+ viewcnt + "]";
+}
+public int getNo() {
+	return no;
+}
+public void setNo(int no) {
+	this.no = no;
+}
+public int getActlocalno() {
+	return actlocalno;
+}
+public void setActlocalno(int actlocalno) {
+	this.actlocalno = actlocalno;
+}
+public int getPupsno() {
+	return pupsno;
+}
+public void setPupsno(int pupsno) {
+	this.pupsno = pupsno;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Date getRegdt() {
+	return regdt;
+}
+public void setRegdt(Date regdt) {
+	this.regdt = regdt;
+}
+public String getLogo() {
+	return logo;
+}
+public void setLogo(String logo) {
+	this.logo = logo;
+}
+public String getIntro() {
+	return intro;
+}
+public void setIntro(String intro) {
+	this.intro = intro;
+}
+public int getMaxcnt() {
+	return maxcnt;
+}
+public void setMaxcnt(int maxcnt) {
+	this.maxcnt = maxcnt;
+}
+public int getFee() {
+	return fee;
+}
+public void setFee(int fee) {
+	this.fee = fee;
+}
+public int getViewcnt() {
+	return viewcnt;
+}
+public void setViewcnt(int viewcnt) {
+	this.viewcnt = viewcnt;
+}
+  
   
  
-
-  
   
 }
