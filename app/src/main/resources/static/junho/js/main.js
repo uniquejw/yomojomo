193c6f3ff2977561570111467622b67a58f206e8
junho-myeong <wnsgh9166@naver.com>
2022-03-31 22:50:28 +0900

    main page

---
+++ app/src/main/resources/static/junho/js/main.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "3",
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation : {
+    prevEl : '.swiper-prev',
+    nextEl : '.swiper-next'
+  }
+});
+
+console.log("연결!")
