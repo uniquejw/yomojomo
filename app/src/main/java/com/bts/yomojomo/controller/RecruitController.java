8846d56489108ed26c1ebc52d7aa77f24255effd
KyoungHyunPark <findkh@nate.com>
2022-04-06 22:34:17 +0900

    log4j
    스프링부트의 기본 로그 사용하기

--- app/src/main/java/com/bts/yomojomo/controller/RecruitController.java
+++ app/src/main/java/com/bts/yomojomo/controller/RecruitController.java
@@ -1,5 +1,8 @@
 package com.bts.yomojomo.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +11,30 @@ import com.bts.yomojomo.service.RecruitService;
 
 @RestController
 public class RecruitController {
+
+  //log를 출력하는 도구 준비
+  private static final Logger log = LoggerFactory.getLogger(RecruitController.class);
+
   @Autowired 
   RecruitService recruiteService;
 
   @RequestMapping("/recruit/list")
   public Object list() {
+    //    log.error("error....");
+    //    log.warn("warn....");
+    //    log.info("info....");
+    //    log.debug("debug....");
+    //    log.trace("trace....");
+
+    log.info("게시물 목록 조회");
     return recruiteService.list();
   }
 
   @RequestMapping("/recruit/add")
   public int add(Recruit recruit) {
+
+    log.info("게시글 등록!"); //운영자가 확인하기를 원하는 정보
+    log.debug(recruit.toString()); //개발자가 확인하기를 원하는 정보
     return recruiteService.add(recruit);
   }
 
