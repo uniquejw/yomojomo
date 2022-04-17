18bf3896326d3b77ec609c33594b46f043fdd263
junho-myeong <wnsgh9166@naver.com>
2022-04-17 11:44:55 +0900

    midpint

---
+++ app/src/main/java/com/bts/yomojomo/controller/MemberMidpointController.java
@@ -0,0 +1,14 @@
+//package com.bts.yomojomo.controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//import com.bts.yomojomo.domain.MemberMidpoint;
+//
+//@RestController
+//public class MemberMidpointController {
+//
+//  @RequestMapping("/junho/memberMidpoint/add")
+//  public Object add(MemberMidpoint memberMidpoint) {
+//
+//  }
+//}
