854855431d2e9ec572c041b9115739b5fa747a76
junho-myeong <wnsgh9166@naver.com>
2022-04-24 21:37:38 +0900

    midpoint update

---
+++ app/src/main/java/com/bts/yomojomo/controller/MemberMidpointSelectController.java
@@ -0,0 +1,22 @@
+package com.bts.yomojomo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import com.bts.yomojomo.domain.Accounting;
+
+@RestController
+public class MemberMidpointSelectController {
+
+  private static final Logger log = LoggerFactory.getLogger(Accounting.class);
+
+  //  @Autowired 
+  //  MemberMidpointSelectService memberMidpointSelectService;
+  //
+  //  @RequestMapping("/accounting/list")
+  //  public Object list() {
+  //    return accountingService.list();
+  //  }
+
+
+}
