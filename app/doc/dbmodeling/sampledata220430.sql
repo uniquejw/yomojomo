de6bbd3b9e372bf688ff7e44d73e2501a4ebc3e1
junho-myeong <wnsgh9166@naver.com>
2022-04-30 21:28:18 +0900

    .

--- app/doc/dbmodeling/sampledata220430.sql
+++ app/doc/dbmodeling/sampledata220430.sql
@@ -268,15 +268,15 @@ insert into gms_main_cate(name) values('공지사항');
 insert into gms_main_cate(name) values('자주하는 질문');
 insert into gms_main_cate(name) values('아무거나');
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
-values('강철민',password('1111'),'aaa@naver.com','11112222','1234','도로명1길','봉천동 어딘가','1012호','1',false);
+values('엄진영',password('1111'),'aaa@naver.com','010-3574-2486','1234','도로명1길','봉천동 어딘가','1012호','1',false);
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
-values('조순옥',password('1111'),'bbb@naver.com','11112222','1234','도로명2길','봉조동 어딘가','1022호','1',false);
+values('박상준',password('1111'),'bbb@naver.com','11112222','1234','도로명2길','봉조동 어딘가','1022호','1',false);
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
-values('이현경',password('1111'),'ccc@naver.com','11112222','1234','도로명3길','봉억동 어딘가','1102호','1',false);
+values('신지섭',password('1111'),'ccc@naver.com','11112222','1234','도로명3길','봉억동 어딘가','1102호','1',false);
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
-values('박요섭',password('1111'),'ddd@naver.com','11112222','1234','도로명4길','봉만동 어딘가','112호','1',false);
+values('김가희',password('1111'),'ddd@naver.com','11112222','1234','도로명1길','봉천동 어딘가','1012호','1',false);
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
-values('조민호',password('1111'),'eee@naver.com','11112222','1234','도로명5길','봉백동 어딘가','112호','1',false);
+values('김이나',password('1111'),'eee@naver.com','11112222','1234','도로명1길','봉천동 어딘가','1012호','1',false);
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
 values('엄상준',password('1111'),'fff@naver.com','11112222','1234','도로명5길','봉백동 어딘가','112호','1',false);
 insert into gms_memb(membername,pwd,email,tel,post_no, roadname_addr ,base_addr,addr, memb_level,unsubscribe) 
